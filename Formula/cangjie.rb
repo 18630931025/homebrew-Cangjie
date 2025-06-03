dd929a53ee755b361987744a4650415be41e3888
18630931025 <18630931025@163.com>
2025-06-03 16:06:35 +0800

    Add Cangjie formula v0.53.18

--- Formula/cangjie.rb
+++ Formula/cangjie.rb
@@ -1,21 +1,32 @@
 class Cangjie < Formula
-  desc "仓颉输入法引擎"
+  desc "Cangjie input method engine"
   homepage "https://github.com/18630931025/cangjie-0.53.18"
-  url "https://github.com/18630931025/cangjie-0.53.18/releases/download/v0.53.18/Cangjie-0.53.18-darwin_aarch64.tar.gz"
-  sha256 "0044b6810ef78b19ec451ef0514a8d4de1634ae36e4680746e89b329b381991e" # 替换为实际的SHA256值
-  license "MIT"
-
-  depends_on "cmake" => :build
-  depends_on "pkg-config" => :build
-  depends_on "glib"
-
-  def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    system "cmake", "--build", "build"
-    system "cmake", "--install", "build"
+  version "0.53.18"
+  license "MIT" # 替换为实际许可证
+  
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/18630931025/cangjie-0.53.18/releases/download/v0.53.18/Cangjie-0.53.18-darwin_aarch64.tar.gz"
+      sha256 "0044b6810ef78b19ec451ef0514a8d4de1634ae36e4680746e89b329b381991e" # 替换为实际值
+    else
+      url "https://github.com/18630931025/cangjie-0.53.18/releases/download/v0.53.18/Cangjie-0.53.18-darwin_x64.tar.gz"
+      sha256 "2ce777c02c5c0c264ce6a2a1bbaf46ed981698c43cd967b096d2ecc116c29801" # 替换为实际值
+    end
   end
-
+  
+  def install
+    # 安装可执行文件
+    bin.install "bin/cangjie"
+    
+    # 安装库文件
+    lib.install Dir["lib/*"]
+    
+    # 安装资源文件（如果有）
+    # share.install "share/cangjie"
+  end
+  
   test do
+    # 测试命令是否正常工作
     system "#{bin}/cangjie", "--version"
   end
 end
